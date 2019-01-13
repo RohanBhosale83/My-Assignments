cc32941f563f54c75d74dbb7b145afc0b3359970
Rohan Bhosale <rohan.bhosale2537@gmail.com>
2019-01-13 13:18:04 +0530

    Update AccountController.java

--- my-spring-mvc-application/src/main/java/com/capgemini/account/controller/AccountController.java
+++ my-spring-mvc-application/src/main/java/com/capgemini/account/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.capgemini.account.controller;
 
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +19,9 @@ import com.capgemini.moneymoney.exception.AccountNotFoundException;
 public class AccountController {
 	@Autowired
 	private SavingsAccountService savingsAccountService ;
-	
 	private List<SavingsAccount> savingsAccounts;
 	private SavingsAccount account;
+	private boolean toSortIn=false;
 	
 	@RequestMapping("/home")
 	public String home() {
@@ -39,81 +42,29 @@ public class AccountController {
 	}
 	
 	@RequestMapping("/updateAccount")
-	public String update() {
-		return "updateaccount";
+	public String searchAccount() {
+		return "updateAccountForm";
 	}
-	
-	@RequestMapping("/updateAccountInfo")
-	public String getAccount(@RequestParam("accountNumber")int accountNumber, Model model) throws AccountNotFoundException {
-		account= savingsAccountService.getAccountById(accountNumber);
+
+	@RequestMapping("/accountToUpdate")
+	public String getAccount(@RequestParam("accountNumber") int accountNumber, Model model) throws ClassNotFoundException, SQLException, AccountNotFoundException {
+		SavingsAccount account = savingsAccountService.getAccountById(accountNumber);
+		 model.addAttribute("account", account);
 		return "AccountDetails";
 	}
 	
-	/*
-	 * @RequestMapping("") public String
-	 * updateAccount(@RequestParam("accountNumber")int
-	 * accountNumber, @RequestParam("")String accountHolderName,
-	 * 
-	 * @RequestParam("accountType")String type) { return type; }
-	 */
-	
-//	@RequestMapping("/updateAccount")
-//	public String updateForm() {
-//		return "updateAccount";
-//	}
-	
-	/*
-	 * @RequestMapping("/updateAccHN") public String getupdateHolderNameForm() {
-	 * return "updateHN"; }
-	 * 
-	 * @RequestMapping("/updateAccountHolderName") public String
-	 * updateHolderName(@RequestParam("accountNumber")int accountNumber,
-	 * 
-	 * @RequestParam("accHolderName")String accountHolderName) throws
-	 * AccountNotFoundException { account
-	 * =savingsAccountService.getAccountById(accountNumber);
-	 * account.getBankAccount().setAccountHolderName(accountHolderName);
-	 * savingsAccountService.updateAccount(account); return
-	 * "redirect:getAllAccounts"; }
-	 */
-	/*
-	 * @RequestMapping("/updateSalary") public String getupdateSalariedForm() {
-	 * return "updateSalaried"; }
-	 * 
-	 * @RequestMapping("/updateSalary") public String
-	 * updateSalaried(@RequestParam("accountNumber")int accountNumber,
-	 * 
-	 * @RequestParam("rdSalary")boolean salary) throws AccountNotFoundException {
-	 * account =savingsAccountService.getAccountById(accountNumber);
-	 * account.setSalary(salary); savingsAccountService.updateAccount(account);
-	 * return "rediect:getAllAccounts"; }
-	 * 
-	 * @RequestMapping("/updateAccHNSal") public String updateNameSalary() { return
-	 * "updateAccount"; }
-	 * 
-	 * @RequestMapping("/updateAccountInfo") public String
-	 * updateAccountDetailsForm(@RequestParam("accountNumber")int accountNumber,
-	 * 
-	 * @RequestParam("accHolderName")String
-	 * accountHolderName,@RequestParam("rdSalary")boolean salary) throws
-	 * AccountNotFoundException { account
-	 * =savingsAccountService.getAccountById(accountNumber);
-	 * account.getBankAccount().setAccountHolderName(accountHolderName);
-	 * account.setSalary(salary); savingsAccountService.updateAccount(account);
-	 * 
-	 * return "redirect:getAllAccounts"; }
-	 */
-	 /* 
-	 * @RequestMapping("/updateAccountInfo") public String
-	 * updateAccountDetails(@RequestParam("accHolderName")String accountHolderName,
-	 * 
-	 * @RequestParam("rdSalary") boolean salary) throws AccountNotFoundException {
-	 * SavingsAccount sa = null; String accHolderName =
-	 * sa.getBankAccount().getAccountHolderName(); boolean salaried = sa.isSalary();
-	 * int accNumber= sa.getBankAccount().getAccountNumber();
-	 * account=savingsAccountService. updateAccount(sa); return
-	 * "redirect:getAllAccounts"; }
-	 */
+	@RequestMapping("/updateAcc")
+	public String updateAccount(@RequestParam("accountNumber") int accountNumber, @RequestParam("newName") String accountHolderName,
+			@RequestParam("accountBalance") double accountBalance,
+			@RequestParam("accountType") String accountType,@RequestParam("newSalaried") String salaried, Model model)
+			throws ClassNotFoundException, SQLException, AccountNotFoundException {
+		
+		boolean salariedOrNot = salaried.equalsIgnoreCase("yes")?true:false;
+		SavingsAccount savingAccountToUpdate = new SavingsAccount(accountNumber, accountHolderName,accountBalance, salariedOrNot);
+		savingAccountToUpdate = savingsAccountService.updateAccount(savingAccountToUpdate);
+		model.addAttribute("account", savingAccountToUpdate);
+		return "AccountDetails";
+	}
 	
 	@RequestMapping("/searchAccount")
 	public String getSearchAccountForm() {
@@ -138,15 +89,6 @@ public class AccountController {
 		return "redirect:getAllAccounts";
 	}
 	
-	@RequestMapping("/sortByName")
-	public String sortAccountsByName() {
-		return "sortAccounts";
-	}
-	
-	@RequestMapping("/sortAll")
-	public String sortAllAccounts() {
-		return "redirect:getAllAccounts";
-	}
 	@RequestMapping("/closeAccount")
 	public String getCloseAccountForm() {
 		return "closeAccount";
@@ -209,5 +151,60 @@ public class AccountController {
 		return "AccountDetails";
 	}
 	
+	@RequestMapping("/sortByName")
+	public String sortByName(Model model) throws ClassNotFoundException, SQLException {
+		
+		List<SavingsAccount> accounts = savingsAccountService.getAllSavingsAccount();
+		toSortIn = !toSortIn;
+		final int sort = toSortIn == false ? 1 : -1;									
+		Collections.sort(accounts, new Comparator<SavingsAccount>() {
+			@Override
+			public int compare(SavingsAccount accountOne, SavingsAccount accountTwo) {
+				return sort* (accountOne.getBankAccount().getAccountHolderName().compareToIgnoreCase(accountTwo.getBankAccount().getAccountHolderName()));
+			}
+		});
+		model.addAttribute("accounts", accounts);
+		return "AccountDetails";
+	}
 	
+	
+	@RequestMapping("/sortAccountsById")
+	public String sortByAccountId(Model model) throws ClassNotFoundException, SQLException {
+		
+		List<SavingsAccount> accounts = savingsAccountService.getAllSavingsAccount();
+		toSortIn = !toSortIn;
+		final int sort = toSortIn == false ? 1 : -1;									
+		Collections.sort(accounts, new Comparator<SavingsAccount>() {
+			@Override
+			public int compare(SavingsAccount accountOne, SavingsAccount accountTwo) {
+				if(accountOne.getBankAccount().getAccountNumber() >accountTwo.getBankAccount().getAccountNumber())
+					return -1*sort;
+				else if(accountOne.getBankAccount().getAccountNumber() < accountTwo.getBankAccount().getAccountNumber())
+					return 1*sort;
+				else
+					return 0;
+			}
+		});
+		model.addAttribute("accounts", accounts);
+		return "AccountDetails";
+	}
+	
+	@RequestMapping("/sortAccountsBySalary")
+	public String sortAccountByIsSalaried(Model model) throws ClassNotFoundException, SQLException {
+		
+		List<SavingsAccount> accounts = savingsAccountService.getAllSavingsAccount();
+		toSortIn = !toSortIn;
+		final int sort = toSortIn == false ? 1 : -1;									
+		Collections.sort(accounts, new Comparator<SavingsAccount>() {
+			@Override
+			public int compare(SavingsAccount accountOne, SavingsAccount accountTwo) {
+				if(accountOne.isSalary() == accountTwo.isSalary())
+					return 0*sort;
+				else
+					return sort*-1;
+			}
+		});
+		model.addAttribute("accounts", accounts);
+		return "AccountDetails";
+	}
 }
